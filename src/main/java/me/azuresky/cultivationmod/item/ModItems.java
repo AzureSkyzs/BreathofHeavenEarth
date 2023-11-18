ca234e7e84acb1a1d5e82340eedec73fd6c4aadc
Quinton <redsythc@gmail.com>
2023-11-18 02:33:22 -0600

    Added Qi Crystal and Elemental Tester
    -Swapped the texture for qi stone to a new one and made the old texture into qi crystal.
    -Added a Elemental tester that does nothing right now but the recipe was created.
    -Made a way to get qi crystals by smelting qi stone

--- src/main/java/me/azuresky/cultivationmod/item/ModItems.java
+++ src/main/java/me/azuresky/cultivationmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package me.azuresky.cultivationmod.item;
 
 import me.azuresky.cultivationmod.CultivationMod;
+import me.azuresky.cultivationmod.item.custom.ElementalTesterItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,8 +15,14 @@ public class ModItems {
     public static final RegistryObject<Item> QISTONE = ITEMS.register("qistone",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
 
+    public static final RegistryObject<Item> QICRYSTAL = ITEMS.register("qicrystal",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
+
     public static final RegistryObject<Item> RAW_QISTONE = ITEMS.register("raw_qistone",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
+
+    public static final RegistryObject<Item> ELEMENTAL_TESTER = ITEMS.register("elemental_tester",
+            () -> new ElementalTesterItem(new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
 
 
 
