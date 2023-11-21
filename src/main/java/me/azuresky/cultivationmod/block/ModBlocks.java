87d2877852b5a891e8e98b695aab3258a7e3b9d0
Quinton <redsythc@gmail.com>
2023-11-20 22:25:36 -0600

    Added Mega Oak Blocks
    Added Mega Planks, Wood, and Logs also added stripped variants

--- src/main/java/me/azuresky/cultivationmod/block/ModBlocks.java
+++ src/main/java/me/azuresky/cultivationmod/block/ModBlocks.java
@@ -1,14 +1,19 @@
 package me.azuresky.cultivationmod.block;
 
 import me.azuresky.cultivationmod.CultivationMod;
+import me.azuresky.cultivationmod.block.custom.ModFlammableRotatedPillerBlock;
 import me.azuresky.cultivationmod.item.ModCreativeModeTab;
 import me.azuresky.cultivationmod.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -53,11 +58,45 @@ public class ModBlocks {
                     .of(Material.STONE)
                     .strength(5f)
                     .requiresCorrectToolForDrops()), ModCreativeModeTab.CULTIVATION_TAB_BLOCKS);
- /*   public static final RegistryObject<Block> MEGA_OAK_PLANKS = registryBlock("mega_oak_planks",
-            () -> new Block(BlockBehaviour.Properties
-                    .of(Material.WOOD)
-                    .strength(5f)
-                    .requiresCorrectToolForDrops()), ModCreativeModeTab.CULTIVATION_TAB_BLOCKS);*/
+    public static final RegistryObject<Block> MEGA_OAK_LOG = registryBlock("mega_oak_log",
+            () -> new ModFlammableRotatedPillerBlock(
+                    BlockBehaviour.Properties
+                    .copy(Blocks.OAK_LOG)), ModCreativeModeTab.CULTIVATION_TAB_BLOCKS);
+    public static final RegistryObject<Block> MEGA_OAK_WOOD = registryBlock("mega_oak_wood",
+            () -> new ModFlammableRotatedPillerBlock(
+                    BlockBehaviour.Properties
+                            .copy(Blocks.OAK_WOOD)), ModCreativeModeTab.CULTIVATION_TAB_BLOCKS);
+
+    public static final RegistryObject<Block> MEGA_OAK_PLANKS = registryBlock("mega_oak_planks",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)) {
+
+                @Override
+                public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return true;
+
+                }
+
+                @Override
+                public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 20;
+                }
+
+                @Override
+                public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 5;
+                }
+
+
+    }, ModCreativeModeTab.CULTIVATION_TAB_BLOCKS);
+
+    public static final RegistryObject<Block> STRIPPED_MEGA_OAK_LOG = registryBlock("stripped_mega_oak_log",
+            () -> new ModFlammableRotatedPillerBlock(
+                    BlockBehaviour.Properties
+                            .copy(Blocks.STRIPPED_OAK_LOG)), ModCreativeModeTab.CULTIVATION_TAB_BLOCKS);
+    public static final RegistryObject<Block> STRIPPED_MEGA_OAK_WOOD = registryBlock("stripped_mega_oak_wood",
+            () -> new ModFlammableRotatedPillerBlock(
+                    BlockBehaviour.Properties
+                            .copy(Blocks.STRIPPED_OAK_WOOD)), ModCreativeModeTab.CULTIVATION_TAB_BLOCKS);
     public static final RegistryObject<Block> MEGA_OAK_BUTTON = registryBlock("mega_oak_button",
             () -> new WoodButtonBlock(BlockBehaviour.Properties
                     .copy(Blocks.OAK_BUTTON)
