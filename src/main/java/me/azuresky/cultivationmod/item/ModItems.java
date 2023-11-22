73643733910d6301d06884fbc565ed6bc535997a
Quinton <redsythc@gmail.com>
2023-11-22 17:26:25 -0600

    Added Qi Crystal Tool and Fixed Mega Oak Recipes

--- src/main/java/me/azuresky/cultivationmod/item/ModItems.java
+++ src/main/java/me/azuresky/cultivationmod/item/ModItems.java
@@ -2,7 +2,7 @@ package me.azuresky.cultivationmod.item;
 
 import me.azuresky.cultivationmod.CultivationMod;
 import me.azuresky.cultivationmod.item.custom.ElementalTesterItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -26,7 +26,25 @@ public class ModItems {
     public static final RegistryObject<Item> MEGA_OAK_ROD = ITEMS.register("mega_oak_rod",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
 
+    public static final RegistryObject<Item> QICRYSTAL_SWORD = ITEMS.register("qicrystal_sword",
+            () -> new SwordItem(ModTiers.QICRYSTAL,4,3f,
+                    new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
 
+    public static final RegistryObject<Item> QICRYSTAL_PICKAXE = ITEMS.register("qicrystal_pickaxe",
+            () -> new PickaxeItem(ModTiers.QICRYSTAL,1,2f,
+                    new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
+
+    public static final RegistryObject<Item> QICRYSTAL_AXE = ITEMS.register("qicrystal_axe",
+            () -> new AxeItem(ModTiers.QICRYSTAL,5,1f,
+                    new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
+
+    public static final RegistryObject<Item> QICRYSTAL_SHOVEL = ITEMS.register("qicrystal_shovel",
+            () -> new ShovelItem(ModTiers.QICRYSTAL,0,2f,
+                    new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
+
+    public static final RegistryObject<Item> QICRYSTAL_HOE = ITEMS.register("qicrystal_hoe",
+            () -> new HoeItem(ModTiers.QICRYSTAL,0,0,
+                    new Item.Properties().tab(ModCreativeModeTab.CULTIVATION_TAB_ITEMS)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
---
+++ src/main/java/me/azuresky/cultivationmod/item/ModTiers.java
@@ -0,0 +1,13 @@
+package me.azuresky.cultivationmod.item;
+
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+
+public class ModTiers {
+
+    public static final ForgeTier QICRYSTAL = new  ForgeTier(3, 2000, 12.5f,
+            4f, 22, BlockTags.NEEDS_DIAMOND_TOOL,
+            () -> Ingredient.of(ModItems.QICRYSTAL.get()));
+
+}
